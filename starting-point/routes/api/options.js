88853a91806ab9c39ae4be06c09d29efa16f456a
Christopher Gupta <gupta.christopher@gmail.com>
2017-03-23 12:11:45 -0400

    some routes added

--- starting-point/routes/api/options.js
+++ starting-point/routes/api/options.js
@@ -35,10 +35,57 @@ router.get('/days', function(req, res, next) {
     res.json(allDays)
   })
   .catch(next)
-
-
 });
 
+router.get('/days/:id', function(req, res, next){
+  Day.findById(req.params.id)
+  .then(function(oneDay){
+    res.json(oneDay)
+  })
+  .catch(next)
+})
 
+router.post('/days', function(req, res, next){
+  Day.create(req.body)
+  .then(function(){
+    res.sendStatus(200)
+  })
+  .catch(next)
+})
+
+router.delete('/days/:id', function(req, res, next){
+  Day.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .catch(next)
+})
+
+router.post('/days/:id/restaurants', function(req, res, next){
+  Day.findById(req.params.id)
+  .then(function(foundDay){
+    foundDay.addRestaurant(req.body)
+  })
+  .catch(next)
+})
+
+router.post('/days/:id/hotels', function(req, res, next){
+  Day.findById(req.params.id)
+  .then(function(foundDay){
+    foundDay.update({
+      hotelId: req.body.id
+    })
+  })
+  .catch(next)
+})
+
+router.post('/days/:id/activities', function(req, res, next){
+  Day.findById(req.params.id)
+  .then(function(foundDay){
+    foundDay.addActivity(req.body)
+  })
+  .catch(next)
+})
 
 module.exports = router;
